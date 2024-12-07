14327e559c491761326913612c6a5d2ba7b67844
Sarthak Deshmukh <sarthakd.work@gmail.com>
2024-12-07 17:53:26 -0500

    Delete api directory

--- api/service/routers/chatbot-router.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import chatbotController from "../controllers/chatbot-controller.js";
-import { authenticateUser } from "../middleware/AuthMiddleware.js";
-
-const router = express.Router();
-
-router.post('/', authenticateUser, chatbotController.chat);
-
-export default router;
