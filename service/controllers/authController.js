a2d48fbb20acc08a7e3fc7ea02be2fad1f0b840b
Sarthak Deshmukh <sarthakd.work@gmail.com>
2024-12-07 18:14:16 -0500

    Update authController.js

--- service/controllers/authController.js
+++ service/controllers/authController.js
@@ -1,19 +1,18 @@
 import authService from './../services/auth-service.js';
-import { setConflict, setServerError, setSuccess, setUnauthorized, setNewRecordCreatedSuccess,setRoleError, setUnprocessableEntity } from './../middleware/response-handler.js';
-import jwt from 'jsonwebtoken'; 
+import { setConflict, setServerError, setSuccess, setUnauthorized, setNewRecordCreatedSuccess, setRoleError, setUnprocessableEntity } from './../middleware/response-handler.js';
+import jwt from 'jsonwebtoken';
 
 class AuthController {
   // Calling the login API
   async login(req, res) {
     try {
-
       const accessToken = await authService.login(req.body);
 
       res.cookie("accessToken", accessToken, {
         httpOnly: true,
         secure: true,
         sameSite: "strict",
-      });  
+      });
 
       const decodedToken = jwt.decode(accessToken);
       setSuccess({
@@ -29,36 +28,38 @@ class AuthController {
     }
   }
 
-  //Calling the signup API
+  // Calling the signup API
   async signup(req, res) {
     try {
-      const token = await authService.signup(req.body);       
-      setNewRecordCreatedSuccess(token,res);
+      const token = await authService.signup(req.body);
+      setNewRecordCreatedSuccess(token, res);
     } catch (error) {
       if (error.message === 'USER_ALREADY_EXISTS') {
         setConflict(res);
-      }else if (error.message === 'INVALID_ROLE') {
+      } else if (error.message === 'INVALID_ROLE') {
         setRoleError(res);
-      } 
-      else if (error.message === 'INVALID_EMAIL') {
+      } else if (error.message === 'INVALID_EMAIL') {
         setUnprocessableEntity(res);
-      } 
-      else {
+      } else {
         setServerError(error, res);
       }
     }
   }
 
-  //Calling the logout API
-  async logout (req, res) {
+  // Calling the logout API
+  async logout(req, res) {
     res.cookie("accessToken", "", {
       httpOnly: true,
       secure: true,
       sameSite: "strict",
     });
     res.status(200).json({ message: 'Logged out successfully' });
-  };
-}
+  }
 
+  // Health check API
+  async health(req, res) {
+    res.status(200).json({ message: 'API working properly.' });
+  }
+}
 
 export default new AuthController();
